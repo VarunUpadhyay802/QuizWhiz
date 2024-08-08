0051025a6c2ea799bc9b862c62a1f023724c77c6
varun__upadhyay <varun.802.vu@gmail.com>
2024-08-08 17:33:30 +0530

    shared link completed

--- src/pages/ScoreBoard/ScoreCardShared.jsx
+++ src/pages/ScoreBoard/ScoreCardShared.jsx
@@ -1,57 +1,72 @@
-import React from "react";
-import { useContext } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import "./Scoreboard.css";
 import { AiOutlineHome, AiOutlineEye } from "react-icons/ai";
 import { BiReset } from "react-icons/bi";
 import { BsShare } from "react-icons/bs";
 import quizContext from "../../context/quizContext";
-import { Link, useParams, useNavigate, useLocation } from "react-router-dom";
-import { toast } from "react-toastify";
-
-import { ToastContainer } from "react-toastify";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import CountUp from "react-countup";
 
 const ScoreCardShared = (props) => {
-//   const context = useContext(quizContext);
-//   const { setNext, setScore, setAnswerList } = context;
-//   const { total_que, correct_que, wrong_que } = props;
-//   let percentage = (correct_que / total_que) * 100;
-//   let Attempted = ((correct_que + wrong_que) / total_que) * 100;
+  const context = useContext(quizContext);
+  const { setNext, setScore, setAnswerList } = context;
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const [totalQue, setTotalQue] = useState(0);
+  const [correctQue, setCorrectQue] = useState(0);
+  const [wrongQue, setWrongQue] = useState(0);
+  const [percentage, setPercentage] = useState(0);
+  const [attempted, setAttempted] = useState(0);
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const total = queryParams.get("total");
+    const correct = queryParams.get("correct");
+    const wrong = queryParams.get("wrong");
+    const perc = queryParams.get("percentage");
+
+    if (total) setTotalQue(total);
+    if (correct) setCorrectQue(correct);
+    if (wrong) setWrongQue(wrong);
+    if (perc) setPercentage(perc);
+
+    // Calculate attempted percentage
+    if (total && correct && wrong) {
+      const attemptedPercentage = ((Number(correct) + Number(wrong)) / Number(total)) * 100;
+      setAttempted(attemptedPercentage);
+    }
+  }, [location.search]);
+
   const handleGoHome = () => {
     navigate("/");
   };
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const total_que = queryParams.get("total");
-  const correct_que = queryParams.get("correct");
-  const wrong_que = queryParams.get("wrong");
-  const percentage = queryParams.get("percentage");
 
-  //   const handlePlayAgain = () => {
-  //     setNext(0);
-  //     setScore({ rightAnswers: 0, wrongAnswers: 0 });
-  //     setAnswerList([]);
-  //   };
+  const handlePlayAgain = () => {
+    setNext(0);
+    setScore({ rightAnswers: 0, wrongAnswers: 0 });
+    setAnswerList([]);
+  };
+
   const generateShareableLink = () => {
-    const shareUrl = `${
-      window.location.origin
-    }/share?total=${total_que}&correct=${correct_que}&wrong=${wrong_que}&percentage=${percentage.toFixed(
+    const shareUrl = `${window.location.origin}/share?total=${totalQue}&correct=${correctQue}&wrong=${wrongQue}&percentage=${percentage.toFixed(
       2
     )}`;
     navigator.clipboard.writeText(shareUrl).then(() => {
-      //   alert("Score URL copied to clipboard!");
       toast.success("Score URL copied to clipboard!");
     });
   };
+
   return (
     <>
       <div className="main">
         <ToastContainer />
-        <div className="score">
-          Your Score <br />
-          <span>
-            {percentage.toFixed(2)} <small>%</small>
+        <div className="score" style={{marginTop:"4px" , display:"flex", flexDirection:"column"}}>
+          <div>Your Score</div>
+          <span style={{fontSize:"33.5px"}}>
+            {Number(percentage).toFixed(2)} %
           </span>
         </div>
         <div className="point-table">
@@ -60,12 +75,12 @@ const ScoreCardShared = (props) => {
               style={{ backgroundColor: "#A45EDA" }}
               className="circle"
             ></div>
-            {/* <div className="mx-2">
+            <div className="mx-2 ">
               <div style={{ color: "#A45EDA" }} className="point">
-                {Attempted.toFixed(2)}%
+                {attempted.toFixed(2)}%
               </div>
               <div className="point-info">Attempted</div>
-            </div> */}
+            </div>
           </div>
           <div className="semi-table">
             <div
@@ -74,7 +89,7 @@ const ScoreCardShared = (props) => {
             ></div>
             <div className="mx-2">
               <div style={{ color: "#A45EDA" }} className="point">
-                {total_que}
+                {totalQue}
               </div>
               <div className="point-info">Total Questions</div>
             </div>
@@ -86,7 +101,7 @@ const ScoreCardShared = (props) => {
             ></div>
             <div className="mx-2">
               <div style={{ color: "rgb(6 143 86)" }} className="point">
-                {correct_que}
+                {correctQue}
               </div>
               <div className="point-info">Correct</div>
             </div>
@@ -98,7 +113,7 @@ const ScoreCardShared = (props) => {
             ></div>
             <div className="mx-2">
               <div style={{ color: "rgb(223 75 75)" }} className="point">
-                {wrong_que}
+                {wrongQue}
               </div>
               <div className="point-info">Wrong</div>
             </div>
@@ -117,22 +132,22 @@ const ScoreCardShared = (props) => {
               <div className="footer-text">Share Score</div>
             </div>
             {/* <div className="text-center">
-              <ReachLink to="/review">
+              <Link to="/review">
                 <div
                   style={{ backgroundColor: "#BF8D6F" }}
                   className="home-btn"
                 >
                   <AiOutlineEye />
                 </div>
-              </ReachLink>
+              </Link>
               <div className="footer-text">Review Answer</div>
-            </div>
+            </div> */}
             <div className="text-center" onClick={handlePlayAgain}>
               <div style={{ backgroundColor: "#5492B3" }} className="home-btn">
                 <BiReset />
               </div>
               <div className="footer-text">Play Again</div>
-            </div> */}
+            </div>
           </div>
         </div>
       </div>
